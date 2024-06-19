ea38b5f392f9feb73bb41b17cc7d58835e15823c
nitish1899 <nitishcyberminds@gmail.com>
2024-06-19 12:11:33 +0530

    added get task using query

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,4 +1,5 @@
 const { Task } = require('../models');
+const { sequelize } = require('../models');
 
 exports.createTask = async (req, res) => {
     const { taskName, description, organizationId, createdBy, assignedTo } = req.body;
@@ -18,3 +19,44 @@ exports.getTasks = async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch tasks' });
     }
 };
+
+exports.getUserTasksGroupedByOrganization = async (req, res) => {
+    const userId = req.params.userId;
+
+    const query = `
+    SELECT 
+        o.organization_name,
+        t.task_id,
+        t.task_name,
+        t.description
+    FROM 
+        Users u
+    JOIN 
+        UserOrganizations uo ON u.user_id = uo.user_id
+    JOIN 
+        Organizations o ON uo.organization_id = o.organization_id
+    JOIN 
+        Tasks t ON o.organization_id = t.organization_id
+    WHERE 
+        u.user_id = :userId
+    ORDER BY 
+        o.organization_name, t.task_name;
+  `;
+
+    try {
+        const tasks = await sequelize.query(query, {
+            replacements: { userId: userId },
+            type: sequelize.QueryTypes.SELECT
+        });
+
+        const groupedTasks = tasks.reduce((result, task) => {
+            (result[task.organization_name] = result[task.organization_name] || []).push(task);
+            return result;
+        }, {});
+
+        res.json(groupedTasks);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch tasks' });
+    }
+};
+
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createTask, getTasks } = require('../controllers/taskController');
+const { createTask, getTasks, getUserTasksGroupedByOrganization } = require('../controllers/taskController');
 const authenticate = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/', authenticate, createTask);
 router.get('/', authenticate, getTasks);
+router.get('/user/:userId/tasks', authenticate, getUserTasksGroupedByOrganization);
 
 module.exports = router;
